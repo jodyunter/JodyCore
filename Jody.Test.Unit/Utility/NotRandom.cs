ac0d7112bb8156b6662fe0a19e2b82844f35ac4a
Jody unterschutz <jody_unterschutz@transcanada.com>
2019-09-06 12:53:14 -0600

    m

---
+++ Jody.Test.Unit/Utility/NotRandom.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jody.Test.Unit.Utility
+{
+    //use this to get predicatable responses
+    public class NotRandom:Random
+    {
+        private int _counter = 0;
+        private readonly List<int> _numberList;
+
+        public NotRandom(List<int> numberList)
+        {
+            _numberList = numberList;
+        }
+        public override int Next(int maxValue)
+        {
+            if (_counter > _numberList.Count)
+            {
+                _counter = 0;
+            }
+
+            return _numberList[_counter];
+        }
+    }
+}
